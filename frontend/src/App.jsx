dedfc557242fd19181b00587d2af0f1d93e293f7
Pankaj Jangid <pgjangid15@gmail.com>
2025-11-20 17:56:04 +0530

    session page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,11 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignIn,
-  SignInButton,
-  SignOutButton,
-  UserButton,
-  useUser,
-} from "@clerk/clerk-react";
-import { Navigate, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage";
-import ProblemsPage from "./pages/ProblemsPage";
+import { useUser } from "@clerk/clerk-react";
 import { Toaster } from "react-hot-toast";
+import { Navigate, Route, Routes } from "react-router";
 import DashboardPage from "./pages/DashboardPage";
+import HomePage from "./pages/HomePage";
 import ProblemPage from "./pages/ProblemPage";
+import ProblemsPage from "./pages/ProblemsPage";
+import SessionPage from "./pages/SessionPage";
 
 function App() {
   const { isSignedIn, isLoaded } = useUser();
@@ -38,6 +31,10 @@ function App() {
         <Route
           path="/problem/:id"
           element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/session/:id"
+          element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />}
         />
       </Routes>
 
